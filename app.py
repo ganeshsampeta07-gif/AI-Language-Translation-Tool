66e7c405cb3ef133014bf1955f09ee7e785e3ba7
ganeshsampeta07-gif <ganeshsampeta07@gmail.com>
2026-08-23 20:28:14 +0530

    Configure project for Render deployment (Gunicorn & port bindings)

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify, render_template
 from flask_cors import CORS
 from deep_translator import GoogleTranslator
 import traceback
+import os
 
 app = Flask(__name__)
 CORS(app)
@@ -42,4 +43,5 @@ def translate():
         return jsonify({'success': False, 'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, host='127.0.0.1', port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
